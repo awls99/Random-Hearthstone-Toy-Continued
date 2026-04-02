295f5efa5f5dbe413668dc80b2f96cacf33b6bca
Sean ODonnell <19527808+sean8300@users.noreply.github.com>
2026-04-01 21:47:46 -0700

    Update AllHearthToyIndex_en.lua
    Also added Lightcalled Hearthstone

--- AllHearthToyIndex_en.lua
+++ AllHearthToyIndex_en.lua
@@ -34,6 +34,7 @@ AllHearthToyIndex.Locales.enUS = {
 	[246565] = { spellId = 1242509, name = "Cosmic Hearthstone"},
 	[263489] = { spellId = 1270583, name = "Naaru's Enfold"},
 	[263933] = { spellId = 1270814, name = "Preyseeker's Hearthstone"},
+	[257736] = { spellId = 1261979, name = "Lightcalled Hearthstone"},
 
 	-- got a bug report that this HS gets stuck if character os not Draenei, disabling for now
 	-- [210455] = { spellId = 438606, name = "Draenic Hologem" },
